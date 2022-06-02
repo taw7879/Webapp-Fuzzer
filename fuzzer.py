4900177da0a7996b504ac6a9aafd7cdf3ad46f25
Thomas <taw7879@rit.edu>
2022-06-01 23:12:45 -0400

    last

--- fuzzer.py
+++ fuzzer.py
@@ -1,25 +1,11 @@
 # AE Fuzzer Project for BodgeIt Store
 
-import socket
 import os
 import time
 import random
+import requests
 
 os.system("cls")
-host = "127.0.0.1"
-port = 8080
-
-#try:
-print("Attempting connection to BodgeIt Store...")
-time.sleep(0.5)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.settimeout(3)
-client_socket.connect((host, port))
-os.system("cls")
-
-print("Successfully connected to BodgeIt Store!\n")
-
-post = "POST /auth HTTP/1.1\r\n"
 
 while True:
     option = int(input("Send random string or SQLi/XSS commands? (1 or 2) "))
@@ -30,24 +16,16 @@ while True:
         for i in range(length):
             j = random.randint(32, 126)
             parameter += chr(j)
-            
-        contentLength = "Content-Length: " + str(len(parameter)) + "\r\n"
-        contentType = "Content-Type: application/x-www-form-urlencoded\r\n"
-        hostHeader = "Host: http://127.0.0.1:8080/bodgeit/\r\n"
-        connectionClose = "Connection: close\r\n"
-        message = post + contentType + contentLength + hostHeader + connectionClose + "\r\n" + parameter
 
         print("\nSending random string of length " + str(length) + " to BodgeIt Store...")
-        client_socket.send(message.encode())
+        pload = {'username':parameter,'password':'password', 'Login':'Login'}
+        r = requests.post("http://127.0.0.1:8080/bodgeit/login.jsp", data=pload)
         print("\nMessage successfully sent..\n")
         time.sleep(1)
 
-        try:
-            msg = client_socket.recv(1024)
-            print("Server:\n" + msg.decode())
-            print("\nString sent: " + parameter)
-        except:
-            print("Server sent no response")
+        print("Server response:\n" + r.text)
+        print("Status Code: " + str(r.status_code))
+
 
     elif option == 2:
         commands = ["something%0Acat%20/etc/passwd", "ping%PROGRAMFILES:~10,-5%IP", "ping%CommonProgramFiles:~10,-18%IP", "%%2727", "%25%27", "`+HERP", "'+'herp",
@@ -57,16 +35,15 @@ while True:
         index = random.randint(0, len(commands)-1)
         command = commands[index]
         #command = commands[len(commands)-1]
+        
         print("\nSending \"" + command + "\"" + " to BodgeIt Store...")
-        client_socket.send(command.encode())
+        r = requests.post("http://127.0.0.1:8080/bodgeit/login.jsp", data=command)
         print("\nCommand successfully sent..\n")
         time.sleep(1)
         
-        try:
-            msg = client_socket.recv(1024)
-            print("Server:\n" + msg.decode())
-        except:
-            print("Server sent no response")
+        print("Server response:\n" + r.text)
+        print("Status Code: " + str(r.status_code))
+
     
     else:
         continue
@@ -77,11 +54,6 @@ while True:
     else:
         os.system("cls")
 
-#except:
-    #os.system("cls")
-    #print("Failed to connect or Connection stopped to BodgeIt Store...")
-    #time.sleep(2)
-    #os.system("cls")
 
 
-client_socket.close()
+
